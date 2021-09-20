5508385fc413fa3d85bbd6cec970ac3bd53fc2e6
Ernesto Suarez Ramirez <ernestosr@ecc.cu>
2021-09-20 15:23:55 -0400

    Tipo estructura

--- src/Form/Admin/EstructuraTipoType.php
+++ src/Form/Admin/EstructuraTipoType.php
@@ -3,26 +3,16 @@
 namespace App\Form\Admin;
 
 use App\Entity\EstructuraTipo;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EstructuraTipoType extends AbstractType
+class EstructuraTipoType extends GrupoType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('codigo')
-            ->add('nombre')
-            ->add('descripcion')
-            ->add('habilitado')
-        ;
-    }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => EstructuraTipo::class,
+            'translation_domain' => 'nomenclador',
+            'attr' => ['class' => 'form-horizontal'],
         ]);
     }
 }
