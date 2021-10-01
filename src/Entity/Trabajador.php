97322ff51c88f12e84a1d93112c783c57ac9da97
Ernesto Suarez Ramirez <ernestosr@ecc.cu>
2021-10-01 12:28:09 -0400

    Falto

--- src/Entity/Trabajador.php
+++ src/Entity/Trabajador.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TrabajadorRepository;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -158,6 +159,11 @@ class Trabajador extends _Entity_
     public function getSexo(): string
     {
         return $this->getPersona()->getSexo();
+    }
+
+    public function getNacimiento(): DateTime
+    {
+        return $this->getPersona()->getNacimiento();
     }
 
     public function setDatoCredencial($username, $password): self
