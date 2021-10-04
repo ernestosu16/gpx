2c3a87823b2fdcb84a4ccf38f7390adfc0c885ee
Ernesto Suarez Ramirez <ernestosr@ecc.cu>
2021-10-04 14:58:13 -0400

    Agregando el editar de localizacion

--- src/Form/Admin/LocalizacionType.php
+++ src/Form/Admin/LocalizacionType.php
@@ -18,42 +18,22 @@ class LocalizacionType extends AbstractType
         $object = $builder->getData();
 
         $builder
-            ->add('parent', EntityType::class, [
-                'class' => Localizacion::class,
-                'label' => 'parent',
-                'attr' => ['class' => 'form-control input-sm'],
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-                'empty_data' => '',
-                'required' => false,
-            ])
-            ->add('codigo', TextType::class, [
-                'attr' => ['class' => 'form-control input-sm'],
-                'label' => 'codigo',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-            ])
-            ->add('nombre', TextType::class, [
-                'attr' => ['class' => 'form-control input-sm'],
-                'label' => 'name',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-            ])
-            ->add('descripcion', TextareaType::class, [
-                'required' => false,
-                'empty_data' => '',
-                'label' => 'descripcion',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-                'attr' => ['class' => 'form-control input-sm'],
-            ])
-            ->add('codigoAduana', TextType::class, [
-                'attr' => ['class' => 'form-control input-sm'],
-                'label' => 'codigo',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-            ])
             ->add('tipo', EntityType::class, [
                 'class' => LocalizacionTipo::class,
                 'label' => 'tipo',
                 'label_attr' => ['class' => 'col-sm-2 control-label'],
                 'attr' => ['class' => 'form-control input-sm'],
                 'disabled' => (bool)$object->getId()
+            ])
+            ->add('nombre', TextType::class, [
+                'attr' => ['class' => 'form-control input-sm'],
+                'label' => 'nombre',
+                'label_attr' => ['class' => 'col-sm-2 control-label'],
+            ])
+            ->add('codigoAduana', TextType::class, [
+                'attr' => ['class' => 'form-control input-sm'],
+                'label' => 'codigo_aduana',
+                'label_attr' => ['class' => 'col-sm-2 control-label'],
             ]);
     }
 
