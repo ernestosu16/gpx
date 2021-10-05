5d5181ec5b2d628e3772c5bcaf45212bffe833c6
Raynel Rodríguez Oliva <raynel@ecc.cu>
2021-10-05 11:10:00 -0400

    Agregando bundle fos_js

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
