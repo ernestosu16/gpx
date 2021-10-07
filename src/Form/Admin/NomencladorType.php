245146cbe9059408ac7680a701088533956618cd
Ernesto Suarez Ramirez <ernestosr@ecc.cu>
2021-10-07 10:52:25 -0400

    Optimiando y arreglando

--- src/Form/Admin/NomencladorType.php
+++ src/Form/Admin/NomencladorType.php
@@ -3,37 +3,15 @@
 namespace App\Form\Admin;
 
 use App\Entity\Nomenclador;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NomencladorType extends AbstractType
+class NomencladorType extends BaseNomencladorType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        /** @var Nomenclador $data */
-        $data = $builder->getData();
-
+        parent::buildForm($builder,$options);
         $builder
-            ->add('codigo', TextType::class, [
-                'label' => 'codigo',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-                'attr' => ['autocomplete' => 'off', 'class' => 'form-control input-sm'],
-            ])
-            ->add('nombre', TextType::class, [
-                'label' => 'nombre',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-                'attr' => ['autocomplete' => 'off', 'class' => 'form-control input-sm'],
-            ])
-            ->add('descripcion', TextareaType::class, [
-                'label' => 'descripcion',
-                'label_attr' => ['class' => 'col-sm-2 control-label'],
-                'empty_data' => '',
-                'required' => false,
-                'attr' => ['class' => 'form-control input-sm'],
-            ])
             ->add('end', null, [
                 'required' => false,
                 'label' => 'end',
