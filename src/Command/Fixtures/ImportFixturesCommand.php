f32e66e7383674121eb410d27ce4e74d54a3ee17
Ar1am <45601122+Ar1am@users.noreply.github.com>
2021-10-12 16:14:26 -0400

    importFixture

--- src/Command/Fixtures/ImportFixturesCommand.php
+++ src/Command/Fixtures/ImportFixturesCommand.php
@@ -8,7 +8,6 @@ use App\Config\Data\Nomenclador\EnvioData;
 use App\Config\Data\Nomenclador\EstructuraTipoData;
 use App\Config\Data\Nomenclador\GrupoData;
 use App\Config\Data\Nomenclador\MenuData;
-use App\Entity\Agencia;
 use App\Entity\Estructura;
 use App\Entity\EstructuraTipo;
 use App\Entity\Grupo;
@@ -70,13 +69,8 @@ final class ImportFixturesCommand extends BaseCommand implements BaseCommandInte
         $section->writeln('  Creando las estructuras');
         $this->configurarEstructura();
 
-<<<<<<< HEAD
-        $section->writeln('  Creando las agencias');
-        $this->configurarAgencias();
-=======
         $section->writeln('  Creando lista de nomencladores');
         $this->configurarNomenclador();
->>>>>>> master
 
         $this->getEntityManager()->commit();
 
@@ -297,32 +291,6 @@ final class ImportFixturesCommand extends BaseCommand implements BaseCommandInte
         }
 
         return $estructura;
-    }
-
-
-    private function configurarAgencias()
-    {
-        /** @var ?Agencia $root */
-        $root = $this->getRepository(Agencia::class)->findOneBy(['codigo' => AgenciaData::code()]);;
-
-        if ($root->getChildren()->count())
-            return;
-
-        $agencia = Yaml::parseFile($this->getKernel()->getProjectDir() . '/src/Config/Fixtures/agencia.yaml');
-        foreach ($agencia['agencias'] as $agencia) {
-            if ($this->getRepository(Agencia::class)->findOneBy(['codigo' => $agencia['codigo']]))
-                continue;
-
-            $agenciaEntity = new Agencia();
-            $agenciaEntity->setRoot($root);
-            $agenciaEntity->setCodigo($agencia['codigo']);
-            $agenciaEntity->setNombre($agencia['nombre']);
-            $agenciaEntity->setDescripcion($agencia['descripcion']);
-
-            $root->addChild($agenciaEntity);
-        }
-        $this->getEntityManager()->persist($root);
-        $this->getEntityManager()->flush();
     }
 
     private function configurarPaises()
