75305f29168717d3429b8a9495b0327b1f95c640
Ernesto Suarez Ramirez <ernestosr@ecc.cu>
2021-10-13 15:40:47 -0400

    Agregando tareas programas

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Cron\CronBundle\CronCronBundle::class => ['all' => true],
 ];
