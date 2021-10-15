a72b7c87aea720f3921a9ac58a6a74352c9afb40
Raynel Rodríguez Oliva <raynel@ecc.cu>
2021-10-15 13:23:27 -0400

    Logica completa de creacion del: -Envio -EnvioTraza -EnvioAnomaliaTraza -EnvioAduana -EnvioAduanaTraza

--- src/Manager/EnvioManager.php
+++ src/Manager/EnvioManager.php
@@ -5,7 +5,11 @@ namespace App\Manager;
 
 use App\Controller\EnvioController;
 use App\Entity\Envio;
+use App\Entity\EnvioAduana;
+use App\Entity\EnvioAduanaTraza;
+use App\Entity\EnvioAnomaliaTraza;
 use App\Entity\EnvioManifiesto;
+use App\Entity\EnvioTraza;
 use App\Entity\Estructura;
 use App\Entity\Localizacion;
 use App\Entity\TrabajadorCredencial;
@@ -16,6 +20,7 @@ use App\Repository\EstructuraTipoRepository;
 use App\Repository\LocalizacionRepository;
 use App\Repository\NomencladorRepository;
 use App\Repository\PaisRepository;
+use App\Utils\EnvioAnomalia;
 use App\Utils\EnvioDireccion;
 use App\Utils\EnvioPreRecepcion;
 use Doctrine\ORM\EntityManagerInterface;
@@ -115,8 +120,13 @@ class EnvioManager extends _Manager_
             /** @var EnvioPreRecepcion $envioPreRecepcion */
             $envioPreRecepcion = $deserializer->deserialize(json_encode($envio),EnvioPreRecepcion::class,'json');
 
+            $fechaActual = new \DateTime();
+
+            /**
+             * Envio
+             */
             $envio = new Envio();
-            $envio->setFechaRecepcion(new \DateTime());
+            $envio->setFechaRecepcion($fechaActual);
             $envio->setCodTracking($envioPreRecepcion->cod_tracking);
             $envio->setPareo($envioPreRecepcion->pareo);
             $envio->setPeso($envioPreRecepcion->peso);
@@ -127,39 +137,88 @@ class EnvioManager extends _Manager_
             $envio->setDestinatario($envioPreRecepcion->destinatario);
             $envio->setRemitente($envioPreRecepcion->remitente);
 
-            //Ver lo del metodo para buscar una agencia por el id
             $envio->setAgencia($this->agenciaRepository->find($envioPreRecepcion->agencia));
 
-            //Ver lo del metodo para ponerle el estado
-            $envio->setEstado($this->nomencladorRepository->findOneByCodigo('APP_ENVIO_ESTADO_RECEPCIONADO'));
+            $estadoRecepcionado = $this->nomencladorRepository->findOneByCodigo('APP_ENVIO_ESTADO_RECEPCIONADO');
+            $envio->setEstado($estadoRecepcionado);
 
-            //Ver lo del metodo para ponerle el pais origen
             $envio->setPaisOrigen($this->paisRepository->find($envioPreRecepcion->pais_origen));
 
-            $envio->setPaisDestino($this->paisRepository->findByCodigoAduana('CUB'));
+            $envio->setPaisDestino($this->paisRepository->getPaisCuba());
 
-            ///Ver lo del metodo para ponerle la empresa
             $envio->setEmpresa($user->getEstructura()->searchParentsByTipo(
                 $this->estructuraTipoRepository->findOneByCodigo('EMPRESA')
             ));
 
-            $envio->setProvincia($this->localizacionRepository->find($envioPreRecepcion->provincia));
+            /** @var Localizacion $provincia */
+            $provincia = $this->localizacionRepository->find($envioPreRecepcion->provincia);
+            $envio->setProvincia($provincia);
 
-            $envio->setMunicipio($this->localizacionRepository->find($envioPreRecepcion->municipio));
+            /** @var Localizacion $municipio */
+            $municipio = $this->localizacionRepository->find($envioPreRecepcion->municipio);
+            $envio->setMunicipio($municipio);
 
-            //Ver lo del metodo para guardar las anomalias en campo json
-            $envio->setAnomalias($envioPreRecepcion->irregularidades);
-
-            //Ver lo del metodo para generar el campo diecciones en el modelo y guardar en campo json
             $envio->setDirecciones($envioPreRecepcion->direcciones);
 
-            //A;adir lo de las trazas_envio , trazas_aduana y envio_aduana
-
             $em->persist($envio);
-            /*$em->persist($trazaEnvio);
+
+            /**
+             * Envio trazas
+             */
+
+            /** @var EnvioTraza $envioTraza */
+            $envioTraza = new EnvioTraza();
+            $envioTraza->setFecha($fechaActual);
+            $envioTraza->setPeso($envioPreRecepcion->peso);
+            $envioTraza->setEnvio($envio);
+            $envioTraza->setEstado($estadoRecepcionado);
+            $envioTraza->setTrabajador($user->getTrabajador());
+            $envioTraza->setEstructuraOrigen($user->getEstructura());
+            $envioTraza->setIp('');
+
+            $em->persist($envioTraza);
+
+            /**
+             * Anomalias del envio
+             */
+
+            foreach ($envioPreRecepcion->irregularidades as $anomalia){
+
+                $envioAnomaliaTraza = new EnvioAnomaliaTraza();
+                $envioAnomaliaTraza->setAnomalia($this->nomencladorRepository->find($anomalia->getId()));
+                $envioAnomaliaTraza->setDescripcion($anomalia->getDescripcion());
+                $envioAnomaliaTraza->setEnvioTraza($envioTraza);
+
+                $em->persist($envioAnomaliaTraza);
+            }
+
+            /**
+             * Envio aduana
+             */
+
+            $envioAduana = new EnvioAduana();
+            $envioAduana->setEnvio($envio);
+            $envioAduana->setCodTracking($envioPreRecepcion->cod_tracking);
+            $envioAduana->setProvinciaAduana($provincia->getCodigoAduana());
+            $envioAduana->setMunicipioAduana($municipio->getCodigoAduana());
+            $envioAduana->setEstado($estadoRecepcionado);
+
             $em->persist($envioAduana);
-            $em->persist($trazaEnvioAduana);
-            */
+
+            /**
+             * Trazas aduana
+             */
+
+            $envioAduanaTraza = new EnvioAduanaTraza();
+            $envioAduanaTraza->setEnvio($envio);
+            $envioAduanaTraza->setFecha($fechaActual);
+            $envioAduanaTraza->setEstado($estadoRecepcionado);
+
+            $em->persist($envioAduanaTraza);
+
+            /**
+             * Cambiar en la tabla envio manifiesto el campo por recepcionado
+             */
         }
 
         $em->flush();
