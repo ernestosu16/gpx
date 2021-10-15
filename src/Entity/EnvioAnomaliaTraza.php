2ae7ad5e8ab36eb34b18ba51c3c2de27e7c81045
Raynel Rodríguez Oliva <raynel@ecc.cu>
2021-10-15 13:17:33 -0400

    Constructors, get/set de cada atributo

--- src/Entity/EnvioAnomaliaTraza.php
+++ src/Entity/EnvioAnomaliaTraza.php
@@ -19,4 +19,56 @@ class EnvioAnomaliaTraza extends _Entity_
     #[ORM\JoinColumn(name: 'envio_traza_id', referencedColumnName: 'id', nullable: false)]
     private EnvioTraza $envio_traza;
 
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescripcion(): string
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * @param string $descripcion
+     */
+    public function setDescripcion(string $descripcion): void
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * @return Nomenclador
+     */
+    public function getAnomalia(): Nomenclador
+    {
+        return $this->anomalia;
+    }
+
+    /**
+     * @param Nomenclador $anomalia
+     */
+    public function setAnomalia(Nomenclador $anomalia): void
+    {
+        $this->anomalia = $anomalia;
+    }
+
+    /**
+     * @return EnvioTraza
+     */
+    public function getEnvioTraza(): EnvioTraza
+    {
+        return $this->envio_traza;
+    }
+
+    /**
+     * @param EnvioTraza $envio_traza
+     */
+    public function setEnvioTraza(EnvioTraza $envio_traza): void
+    {
+        $this->envio_traza = $envio_traza;
+    }
+
 }
