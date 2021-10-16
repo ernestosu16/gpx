100cd6578f139c4720390da01badeadf5bac6a1e
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2021-10-16 07:28:41 -0400

    Primer test con el controlador de agencia

--- tests/Controller/Admin/AgenciaControllerTest.php
+++ tests/Controller/Admin/AgenciaControllerTest.php
@@ -2,26 +2,86 @@
 
 namespace App\Tests\Controller\Admin;
 
-use App\Entity\Trabajador;
+use App\Entity\Agencia;
 use App\Repository\TrabajadorCredencialRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AgenciaControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    private \Symfony\Bundle\FrameworkBundle\KernelBrowser $client;
+
+    protected function setUp(): void
     {
         $client = static::createClient();
 
-        // retrieve the test user
+        # retrieve the test user
         $testUser = static::getContainer()->get(TrabajadorCredencialRepository::class)->findOneByUsuario('admin');
 
-        // simulate $testUser being logged in
-        $client->loginUser($testUser);
+        # simulate $testUser being logged in
+        $this->client = $client->loginUser($testUser);
+    }
 
-        $crawler = $client->request('GET', '/admin/agencia/');
+    public function testLista(): void
+    {
+        $crawler = $this->client->request('GET', '/admin/agencia/');
 
         $this->assertResponseIsSuccessful();
         $this->assertEquals(1, $crawler->filter('div[class="panel-heading hbuilt"]')->count());
         $this->assertSelectorTextContains('div', 'Sistema de Gestión Paquetería Express Bienvenido al Sistema de Gestión Paquetería Express');
+    }
+
+    public function testCrear(): void
+    {
+        $crawler = $this->client->request('GET', '/admin/agencia/new');
+        $buttonCrawlerNode = $crawler->selectButton('Guardar');
+
+        $form = $buttonCrawlerNode->form();
+
+        # set values on a form object
+        $form['agencia[codigo]'] = 'agencia_symfony';
+        $form['agencia[nombre]'] = 'symfony';
+        $form['agencia[descripcion]'] = 'Symfony rocks!';
+        $form['agencia[habilitado]'] = true;
+
+        # submit the Form object
+        $this->client->submit($form);
+
+        $this->assertEquals(303, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEditar(): void
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $agencia = $em->getRepository(Agencia::class)->findOneByCodigo('agencia_symfony');
+        $crawler = $this->client->request('GET', '/admin/agencia/' . $agencia->getId() . '/edit');
+        $buttonCrawlerNode = $crawler->selectButton('Guardar');
+
+        $form = $buttonCrawlerNode->form();
+
+        # set values on a form object
+        $form['agencia[codigo]'] = 'agencia_symfony';
+        $form['agencia[nombre]'] = 'symfony x';
+        $form['agencia[descripcion]'] = 'Symfony rocks!';
+        $form['agencia[habilitado]'] = true;
+
+        # submit the Form object
+        $this->client->submit($form);
+
+        $this->assertEquals(303, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEliminar(): void
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $agencia = $em->getRepository(Agencia::class)->findOneByCodigo('agencia_symfony');
+        $crawler = $this->client->request('GET', '/admin/agencia/' . $agencia->getId() . '/edit');
+        $buttonCrawlerNode = $crawler->selectButton('Eliminar');
+
+        $form = $buttonCrawlerNode->form();
+
+        # submit the Form object
+        $this->client->submit($form);
+
+        $this->assertEquals(303, $this->client->getResponse()->getStatusCode());
     }
 }
