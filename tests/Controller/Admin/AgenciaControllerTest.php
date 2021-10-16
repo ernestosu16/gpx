76f11695b92fc0688f3b636719c93fc388a3a0a4
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2021-10-16 08:19:36 -0400

    Agregando algunos test de trabajadores

--- tests/Controller/Admin/AgenciaControllerTest.php
+++ tests/Controller/Admin/AgenciaControllerTest.php
@@ -3,28 +3,12 @@
 namespace App\Tests\Controller\Admin;
 
 use App\Entity\Agencia;
-use App\Repository\TrabajadorCredencialRepository;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AgenciaControllerTest extends WebTestCase
+class AgenciaControllerTest extends AdminWebTestCase
 {
-    private \Symfony\Bundle\FrameworkBundle\KernelBrowser $client;
-
-    protected function setUp(): void
-    {
-        $client = static::createClient();
-
-        # retrieve the test user
-        $testUser = static::getContainer()->get(TrabajadorCredencialRepository::class)->findOneByUsuario('admin');
-
-        # simulate $testUser being logged in
-        $this->client = $client->loginUser($testUser);
-    }
-
     public function testLista(): void
     {
         $crawler = $this->client->request('GET', '/admin/agencia/');
-
         $this->assertResponseIsSuccessful();
         $this->assertEquals(1, $crawler->filter('div[class="panel-heading hbuilt"]')->count());
         $this->assertSelectorTextContains('div', 'Sistema de Gestión Paquetería Express Bienvenido al Sistema de Gestión Paquetería Express');
@@ -34,7 +18,6 @@ class AgenciaControllerTest extends WebTestCase
     {
         $crawler = $this->client->request('GET', '/admin/agencia/new');
         $buttonCrawlerNode = $crawler->selectButton('Guardar');
-
         $form = $buttonCrawlerNode->form();
 
         # set values on a form object
@@ -55,7 +38,6 @@ class AgenciaControllerTest extends WebTestCase
         $agencia = $em->getRepository(Agencia::class)->findOneByCodigo('agencia_symfony');
         $crawler = $this->client->request('GET', '/admin/agencia/' . $agencia->getId() . '/edit');
         $buttonCrawlerNode = $crawler->selectButton('Guardar');
-
         $form = $buttonCrawlerNode->form();
 
         # set values on a form object
@@ -76,7 +58,6 @@ class AgenciaControllerTest extends WebTestCase
         $agencia = $em->getRepository(Agencia::class)->findOneByCodigo('agencia_symfony');
         $crawler = $this->client->request('GET', '/admin/agencia/' . $agencia->getId() . '/edit');
         $buttonCrawlerNode = $crawler->selectButton('Eliminar');
-
         $form = $buttonCrawlerNode->form();
 
         # submit the Form object
