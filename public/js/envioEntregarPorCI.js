d28e1b48ff4f3b9c49f7404631effd2547a8a56e
Raynel Rodríguez Oliva <raynel@ecc.cu>
2021-10-24 19:23:29 -0400

    Creando el metodo de busca por CI en el js correspondiente

---
+++ public/js/envioEntregarPorCI.js
@@ -0,0 +1,12 @@
+/**
+ * Buscar envios de una persona por CI
+ * (Envios de una persona, con
+ * estrucutra de destino la del user autenticado &&
+ * en estado recepcionado &&
+ * envio_aduana.datos_despacho != null)
+ */
+function buscarEnvioPreRecepcion()
+{
+
+
+}
