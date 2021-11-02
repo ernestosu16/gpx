d2bda2b13eb0a575d01bd4e4865cd8e0490cca27
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2021-11-02 11:15:34 -0400

    Agregando logo

---
+++ src/Manager/EstructuraManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Manager;
+
+use App\Repository\EstructuraRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class EstructuraManager extends _Manager_
+{
+    public function __construct(
+        private EstructuraRepository $estructuraRepository
+    )
+    {
+    }
+
+    public function obtenerNuestrosMejoresClientes(): Collection
+    {
+        $qb = $this->estructuraRepository
+            ->createQueryBuilder('e')
+            ->where('e.level IN (:level)')->setParameter('level', [0, 1, 2])
+            ->setFirstResult(6);
+
+        $result = $qb->getQuery()->getResult();
+        return new ArrayCollection($result);
+    }
+}
