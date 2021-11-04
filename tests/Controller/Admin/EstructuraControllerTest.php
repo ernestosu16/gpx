1e89cb4065b8b23ea45cd86b3b28124d7355befa
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2021-11-04 16:43:13 -0400

    Quitando el filtro porq se agregara despues

--- tests/Controller/Admin/EstructuraControllerTest.php
+++ tests/Controller/Admin/EstructuraControllerTest.php
@@ -11,8 +11,8 @@ class EstructuraControllerTest extends AdminWebTestCase
         $crawler = $this->client->request('GET', '/admin/estructura/');
         $this->assertResponseIsSuccessful();
         $this->assertEquals(3, $crawler->filter('li[class="dd-item"]')->count());
-        $this->assertSelectorExists('select[name="filterField"]');
-        $this->assertSelectorExists('input[name="filterValue"]');
+//        $this->assertSelectorExists('select[name="filterField"]');
+//        $this->assertSelectorExists('input[name="filterValue"]');
         $this->assertSelectorTextContains('button[class="btn btn-sm btn-success btn-block"]', 'Filtrar');
         $this->assertSelectorTextContains('div[class="panel-heading hbuilt"]', 'Listado de las estructuras');
     }
