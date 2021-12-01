c0c759f21448c99769df7d6cadfb05304d6ed7d3
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2021-12-01 15:58:40 -0500

    Actualizando y arreglando ID UUID

--- src/Entity/_Entity_.php
+++ src/Entity/_Entity_.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 
 abstract class _Entity_
 {
     #[ORM\Id]
     #[ORM\Column(type: 'string', length: 36)]
-    #[ORM\GeneratedValue(strategy: 'UUID')]
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
     #[Groups(['default'])]
     protected ?string $id = null;
 
