c6494cdb24a0c91570e4483808895734c91c047b
Ernesto Suárez Ramírez <ernestosr@ecc.cu>
2022-01-25 10:18:52 -0500

    Agregando orden a los grupos

--- src/Form/Admin/BaseAdminType.php
+++ src/Form/Admin/BaseAdminType.php
@@ -6,11 +6,13 @@ use App\Entity\Estructura;
 use App\Entity\TrabajadorCredencial;
 use App\Repository\EstructuraRepository;
 use App\Repository\Nomenclador\GrupoRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use function Symfony\Component\String\u;
 
 abstract class BaseAdminType extends AbstractType
 {
@@ -62,11 +64,36 @@ abstract class BaseAdminType extends AbstractType
         $grupos = [];
         foreach ($estructuras as $estructura) {
             foreach ($estructura->getTipos() as $tipo) {
-                $grupos = array_merge($grupos, $tipo->getGrupos()->toArray());
+                if ($tipo->getChildren()->count() > 0) {
+                    $grupos = $this->getTiposChildrenGrupos($grupos, $tipo->getChildren());
+                } else {
+                    $grupos = array_merge($grupos, $tipo->getGrupos()->toArray());
+                }
             }
             $grupos = array_merge($grupos, $estructura->getGrupos()->toArray());
         }
-
+        usort($grupos, $this->objectSorter('nombre', 'ASC'));
         return array_unique($grupos);
+    }
+
+    private function getTiposChildrenGrupos(mixed $grupos, Collection $tipos)
+    {
+        foreach ($tipos as $tipo) {
+            if ($tipo->getChildren()->count() > 0) {
+                $grupos = $this->getTiposChildrenGrupos($grupos, $tipo->getChildren());
+            } else {
+                $grupos = array_merge($grupos, $tipo->getGrupos()->toArray());
+            }
+        }
+
+        return $grupos;
+    }
+
+    function objectSorter(string $clave, $orden = null): \Closure
+    {
+        return function ($a, $b) use ($clave, $orden) {
+            $getter = (string)u($clave)->title()->ensureStart('get');
+            return ($orden == "DESC") ? strnatcmp($b->$getter(), $a->$getter()) : strnatcmp($a->$getter(), $b->$getter());
+        };
     }
 }
